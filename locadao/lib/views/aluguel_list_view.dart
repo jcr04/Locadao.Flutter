a7d50c8007144b3e866c1861f9d65db3be722cec
jcr04 <joaocarlosreboucas15@gmail.com>
2024-10-23 19:43:18 -0300

    ajuste logo header

--- locadao/lib/views/aluguel_list_view.dart
+++ locadao/lib/views/aluguel_list_view.dart
@@ -61,7 +61,7 @@ class _AluguelListViewState extends State<AluguelListView> {
                         ),
                         child: VehicleFeatureCardWidget(
                           icon: Icons.car_rental,
-                          title: 'Aluguel: ${aluguel.veiculo}',
+                          title: 'Aluguel ID: ${aluguel.id}',
                           subtitle: 'Status: ${aluguel.status}',
                           gradientStartColor: Colors.deepPurple,
                           gradientEndColor: Colors.purple,
--- locadao/lib/views/home_page.dart
+++ locadao/lib/views/home_page.dart
@@ -20,81 +20,77 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.deepPurple,
       ),
-      body: Stack(
+      body: Column(
         children: [
-          Container(
-            color: Colors.deepPurple, // Cor de fundo
-            height: 200, // Altura da área do cabeçalho
-          ),
-          Positioned(
-            top: 20, // Ajuste a distância do topo conforme necessário
-            left: 20, // Ajuste a distância da esquerda conforme necessário
-            child: Image.asset('lib/assets/locadao.png',
-                height: 150, // Ajuste o tamanho do logo conforme necessário
-                fit: BoxFit.cover,
-                alignment: Alignment.topLeft),
-          ),
-          Column(
-            children: [
-              const SizedBox(
-                  height: 200), // Espaço para deixar abaixo do header
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.all(screenWidth * 0.07),
-                  child: GridView.count(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: screenWidth * 0.035,
-                    mainAxisSpacing: screenWidth * 0.035,
-                    children: <Widget>[
-                      MenuCardWidget(
-                        title: 'Agências',
-                        icon: Icons.location_city,
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const AgenciaListView()),
-                          );
-                        },
-                      ),
-                      MenuCardWidget(
-                        title: 'Veículos',
-                        icon: Icons.directions_car,
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const VeiculoListView()),
-                          );
-                        },
-                      ),
-                      MenuCardWidget(
-                        title: 'Aluguéis',
-                        icon: Icons.car_rental,
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const AluguelListView()),
-                          );
-                        },
-                      ),
-                      MenuCardWidget(
-                        title: 'Clientes',
-                        icon: Icons.person,
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const ClienteListView()),
-                          );
-                        },
-                      ),
-                    ],
-                  ),
+          SizedBox(
+            height: 200,
+            width: double.infinity,
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Image.asset(
+                  'lib/assets/locadao.png',
+                  height: 50,
                 ),
               ),
-            ],
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(screenWidth * 0.07),
+              child: GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: screenWidth * 0.035,
+                mainAxisSpacing: screenWidth * 0.035,
+                children: <Widget>[
+                  MenuCardWidget(
+                    title: 'Agências',
+                    icon: Icons.location_city,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const AgenciaListView()),
+                      );
+                    },
+                  ),
+                  MenuCardWidget(
+                    title: 'Veículos',
+                    icon: Icons.directions_car,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const VeiculoListView()),
+                      );
+                    },
+                  ),
+                  MenuCardWidget(
+                    title: 'Aluguéis',
+                    icon: Icons.car_rental,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const AluguelListView()),
+                      );
+                    },
+                  ),
+                  MenuCardWidget(
+                    title: 'Clientes',
+                    icon: Icons.person,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ClienteListView()),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
