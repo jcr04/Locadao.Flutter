6596a4ed0986e6faeeaf6cbfa8b4125e15e02339
jcr04 <joaocarlosreboucas15@gmail.com>
2024-10-23 19:01:49 -0300

    ajuste tela

--- locadao/lib/widgets/logo_header.dart
+++ locadao/lib/widgets/logo_header.dart
@@ -8,7 +8,7 @@ class ImageHeaderWidget extends StatelessWidget {
   const ImageHeaderWidget({
     super.key,
     required this.imagePath,
-    this.height = 100.0,
+    this.height = 50.0,
     this.backgroundColor = const Color.fromARGB(255, 70, 17, 131),
   });
 
@@ -21,7 +21,7 @@ class ImageHeaderWidget extends StatelessWidget {
       child: Center(
         child: Image.asset(
           imagePath,
-          fit: BoxFit.contain,
+          fit: BoxFit.cover,
           alignment: Alignment.center,
         ),
       ),
